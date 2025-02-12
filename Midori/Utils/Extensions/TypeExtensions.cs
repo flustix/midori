348e1e07dc0a139ba20a231b7c24d48584e6e7bb
flustix <me@flux.moe>
2025-02-12 15:31:40 +0100

    add a nullable type check

---
+++ Midori/Utils/Extensions/TypeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace Midori.Utils.Extensions;
+
+public static class TypeExtensions
+{
+    public static bool IsNullable(this ParameterInfo parameter)
+    {
+        if (Nullable.GetUnderlyingType(parameter.ParameterType) != null)
+            return true;
+
+        var ctx = new NullabilityInfoContext().Create(parameter);
+        return ctx.WriteState == NullabilityState.Nullable || ctx.ReadState == NullabilityState.Nullable;
+    }
+}
