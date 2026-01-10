0e1f86ed28433b225f8a010cf38667ec10fd5f82
flustix <me@flux.moe>
2026-01-10 01:15:44 +0100

    add @ route to testing

---
+++ Midori.Tests/API/AtRoute.cs
@@ -0,0 +1,15 @@
+﻿using Midori.API.Components;
+using Midori.Networking;
+
+namespace Midori.Tests.API;
+
+public class AtRoute : IAPIRoute<APIInteraction>
+{
+    public string RoutePath => "/@me";
+    public HttpMethod Method => HttpMethod.Get;
+
+    public async Task Handle(APIInteraction interaction)
+    {
+        await interaction.ReplyMessage(HttpStatusCode.OK, "ok");
+    }
+}
